52a87d50abe981ffa785ead777fbb18143fd9f80
carlos123asd <carlos-medin@hotmail.com>
2026-05-09 18:05:38 +0200

    add: spinners for load

--- src/components/organism/ListProducts.tsx
+++ src/components/organism/ListProducts.tsx
@@ -1,12 +1,23 @@
 import { useProductsQuery } from "@/hooks/useProductsQuery";
 import CardProduct from "../molecules/CardProduct";
+import { showToastify } from "@/utils/ShowToastify";
+import { GridLoader } from "react-spinners";
 
 export default function ListProducts({ search }: { search: string }) {
     const { products, isLoading, error } = useProductsQuery();
+    
+    if (error) {
+        console.error("Error fetching products:", error);
+        showToastify("Error loading products", "error").showToast();
+    }
 
-    if (isLoading) return <div>Loading...</div>;
-
-    if (error) return <div>Error: {error.message}</div>;
+    if (isLoading) {
+        return (
+            <div style={styles.loaderContainer}>
+                <GridLoader  color="#1E3A8A" size={100} />
+            </div>
+        );
+    }
 
     return (
         <div className="ListaProductos">
@@ -15,4 +26,13 @@ export default function ListProducts({ search }: { search: string }) {
             ))}
         </div>
     )
+}
+
+const styles = {
+    loaderContainer: {
+        position: "absolute",
+        top: "65%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+    } as React.CSSProperties,
 }
--- src/pages/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.tsx
@@ -2,25 +2,25 @@ import Header from "@/components/organism/Header";
 import CardDetails from "./components/CardDetails/CardDetails";
 import { useParams } from "react-router-dom";
 import { useProductDetailsQuery } from "@/hooks/useGetProductDetailsQuery";
+import { showToastify } from "@/utils/ShowToastify";
+import { PacmanLoader } from "react-spinners";
 
 export default function ProductDetailsPage() {
     const { id } = useParams<{ id: string }>();
     const { details, isLoading, error } = useProductDetailsQuery(id ?? "");
-
+    
     if (error) {
-        return (
-            <div>
-                <Header />
-                <p>Error loading product details: {error.message}</p>
-            </div>
-        );
+        console.error("Error fetching product details:", error);
+        showToastify("Error loading product details", "error").showToast();
     }
 
     if (isLoading) {
         return (
             <div>
                 <Header />
-                <p>Loading product details...</p>
+                <div style={styles.loaderContainer}>
+                    <PacmanLoader color="#1E3A8A" size={150} />
+                </div>
             </div>
         );
     }
@@ -31,4 +31,13 @@ export default function ProductDetailsPage() {
             {details && <CardDetails data={details} />}
         </div>
     )
+}
+
+const styles = {
+    loaderContainer: {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+    } as React.CSSProperties,
 }
