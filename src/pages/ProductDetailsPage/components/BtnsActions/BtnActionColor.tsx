795b8cfe957d418127c07ebacb10ef557d2a4ef6
carlos123asd <carlos-medin@hotmail.com>
2026-05-09 15:53:21 +0200

    fix: color option - component BtnActionColor

--- src/pages/ProductDetailsPage/components/BtnsActions/BtnActionColor.tsx
+++ src/pages/ProductDetailsPage/components/BtnsActions/BtnActionColor.tsx
@@ -1,13 +1,12 @@
 import TextAtom from "@/components/atoms/TextAtom";
 import type { ProductOption } from "@/types/Product";
-import { getHexadecimalFormatFromDecimal } from "@/utils/StringUtils";
 
 export default function BtnActionColor({ option, selected, onClick }: { option: ProductOption, selected: boolean, onClick: () => void }) {
     return (
         <button
             className={`${selected ? "BtnSelected" : ""} BtnNormalActionColor`}
             onClick={onClick}>
-                <div className="CircleColor" style={{ backgroundColor: getHexadecimalFormatFromDecimal(option.code) }}></div>
+                <div className="CircleColor" style={{ backgroundColor: option.name }}></div>
                 <TextAtom text={option.name} color="primary" />
         </button>
     )
--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -2,8 +2,3 @@ export const capitalizar = (str: string): string => {
     if (!str) return "";
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
-
-export const getHexadecimalFormatFromDecimal = (code: number | string): string => {
-    const hexCode = Number(code).toString(16).padStart(6, '0');
-    return `#${hexCode}`;
-}
